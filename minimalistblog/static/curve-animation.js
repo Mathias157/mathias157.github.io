62668130232924713309836b6d807556c02df328
Mathias157 <mathiasros@gmail.com>
2024-07-23 13:03:08 +0200

    A more simple approach, inline svg is recommended according to frontendmaster course

--- minimalistblog/static/curve-animation.js
+++ minimalistblog/static/curve-animation.js
@@ -1,40 +1,12 @@
-// gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin)
+// Using java
+const curve1 = document.querySelector('#Curve')
 
-document.querySelector('.curve1').addEventListener('load', function() {
+// Move slightly
+gsap.set(curve1, {
+    xPercent: 0
+})
 
-    const svgDoc = this.contentDocument
-    
-    // Set svg file to no opacity
-    const g1 = svgDoc.querySelector('path')
-    
-    g1.style.overflow = "visible"
-
-    // Timeline
-    const t1 = gsap.timeline({defaults: {ease: 'power4.out', duration: .7}})
-
-    // Set visibility
-    gsap.set(g1, {
-        autoAlpha: 1
-    })
-
-    // Move slightly
-    gsap.set(g1, {
-        xPercent: 0
-    })
-
-    gsap.to(g1, {
-        scale: 1.5,
-        duration: 2
-    })
-
-    // Draw path
-
-    // gsap.fromTo(g1, {
-    //     drawSVG: "0%"
-    // }, {
-    //     duration: 2,
-    //     drawSVG: "100%",
-    //     ease: "power4.out"
-    // })
-
+gsap.to(curve1, {
+    scale: 1.5,
+    duration: 2
 })
