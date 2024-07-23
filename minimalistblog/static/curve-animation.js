e09b24d9bc17fc0b4d500c74b7a498e8647dcea0
Mathias157 <mathiasros@gmail.com>
2024-07-23 13:14:26 +0200

    Scaling while accounting for mask

--- minimalistblog/static/curve-animation.js
+++ minimalistblog/static/curve-animation.js
@@ -1,7 +1,16 @@
-// Using java
-const curve1 = document.querySelector('#Curve')
+// Get svg
+const svg1 = document.getElementById('mySVG')
 
-// Move slightly
+// Change clip path
+const clippath = svg1.getElementById("_clip1")
+const cliprect = clippath.querySelector('rect')
+cliprect.setAttribute('height', '360')
+cliprect.setAttribute('width', '2423')
+
+// Get content
+const curve1 = svg1.querySelector('path')
+
+// Scale content slightly
 gsap.set(curve1, {
     xPercent: 0
 })
