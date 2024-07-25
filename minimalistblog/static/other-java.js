80fc2e1935f9d78eb82dbbbd0f71d8bfc4746f6c
Mathias157 <mathiasros@gmail.com>
2024-07-25 16:20:21 +0200

    Make all elements invisible until javascript is loaded, preventing "blinks"

--- minimalistblog/static/other-java.js
+++ minimalistblog/static/other-java.js
@@ -1,32 +1,35 @@
 var $circle_init_scale = 1.25;
 
-// Make post invisible
-function makeInvisible(post_id) {
-    const post = document.getElementById(post_id)
-    post.style.visibility = 'hidden';
-}
-
-// Add hover effect to all elements with IDs in the form Post1, Post2, ..., PostN
-// const posts = document.querySelector('#Posts')
-const posts = [];
-for (let i = 1; i < 6; i++) {
-    let post = document.querySelector(`#Post${i} circle`);
-
-    post.style.visibility = 'visible';
-    posts.push(post);
-
-    post.addEventListener('mouseover', function() {
-        gsap.to(post, {scale: $circle_init_scale + 0.7, duration: 0.3, transformOrigin: "center"});
+// Wait for the SVG to load before applying effects
+document.addEventListener('DOMContentLoaded', function() {
+    // Make all elements visible
+    const all_elements = Array.from(document.querySelectorAll('g'));
+    all_elements.forEach(element => {
+        element.style.visibility = 'visible';
     })
-    
-    post.addEventListener('mouseout', function() {
-        gsap.to(post, {scale: $circle_init_scale, duration: 0.3, transformOrigin: "center"});
-        // gsap.to(post, {scale: 3.14/2, duration: 0.3, transformOrigin: "center", attr: {style : 'fill:#00afb9;'}});
-    })
-}
 
-// Initial animation to make the posts appear with a staggered effect
-gsap.fromTo(posts, 
-    { scale: 0, visibility: 'visible', transformOrigin: "center" }, 
-    { scale: $circle_init_scale, duration: 1, transformOrigin: "center", stagger: 0.5 }
-);
+    // Add hover effect to all elements with IDs in the form Post1, Post2, ..., PostN
+    // const posts = document.querySelector('#Posts')
+    const posts = [];
+    for (let i = 1; i < 6; i++) {
+        let post = document.querySelector(`#Post${i} circle`);
+
+        post.style.visibility = 'visible';
+        posts.push(post);
+
+        post.addEventListener('mouseover', function() {
+            gsap.to(post, {scale: $circle_init_scale + 0.7, duration: 0.3, transformOrigin: "center"});
+        })
+        
+        post.addEventListener('mouseout', function() {
+            gsap.to(post, {scale: $circle_init_scale, duration: 0.3, transformOrigin: "center"});
+            // gsap.to(post, {scale: 3.14/2, duration: 0.3, transformOrigin: "center", attr: {style : 'fill:#00afb9;'}});
+        })
+    }
+
+    // Initial animation to make the posts appear with a staggered effect
+    gsap.fromTo(posts, 
+        { scale: 0, visibility: 'visible', transformOrigin: "center" }, 
+        { scale: $circle_init_scale, duration: 1, transformOrigin: "center", stagger: 0.5 }
+    );
+})
