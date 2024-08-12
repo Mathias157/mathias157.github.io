f0b4ce1c278e271165d552987c0b4fcde2fac09c
Mathias157 <mathiasros@gmail.com>
2024-08-12 20:45:04 +0200

    Improved tree animation

--- minimalistblog/static/curve-animation.js
+++ minimalistblog/static/curve-animation.js
@@ -1,37 +1,27 @@
 import anime from "https://cdn.skypack.dev/animejs@3.2.1";
 
-const tree_branches = document.querySelectorAll('.container g #Tree > #Branches path');
-tree_branches.forEach(element => {
-  element.style.visibility = 'hidden';
-});
 
-anime({
+anime.timeline() // Create a timeline for parallel animations
+.add({
   targets: ".container g #Tree > #Treetrunk > path",
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: "linear",
   duration: 3000,
   delay: function (el, i) {
-    return i * 350;
+    return i * 0;
   },
   direction: "alternate",
   loop: false,
-  complete: function() {
-    // Add your next animation here
-    tree_branches.forEach(element => {
-      element.style.visibility = 'visible';
-    });
-
-    anime({
-
-      targets: ".container g #Tree > #Branches path",
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: "linear",
-      duration: 500,
-      delay: function (el, i) {
-        return i * 350;
-      },
-      direction: "alternate",
-      loop: false,
-    });
-  }
-});
+}, 0) // The start of the timeline
+.add({
+  // The order of the animations depend on the order of the Branches paths in the inline svg
+  targets: ".container g #Tree > #Branches path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "linear",
+  duration: 500,
+  delay: function (el, i) {
+    return i * 700 // delay in the animation between each target
+  },
+  direction: "alternate",
+  loop: false,
+}, 500); // The start of the timeline delayed by 500 ms
