fd89e857fed77174410c772885c0b16413144dec
Mathias157 <mathiasros@gmail.com>
2024-08-12 20:31:42 +0200

    Made cute branch animation

--- minimalistblog/static/curve-animation.js
+++ minimalistblog/static/curve-animation.js
@@ -1,15 +1,37 @@
 import anime from "https://cdn.skypack.dev/animejs@3.2.1";
 
-
+const tree_branches = document.querySelectorAll('.container g #Tree > #Branches path');
+tree_branches.forEach(element => {
+  element.style.visibility = 'hidden';
+});
 
 anime({
-  targets: ".container g #Tree path",
+  targets: ".container g #Tree > #Treetrunk > path",
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: "linear",
-  duration: 3500,
+  duration: 3000,
   delay: function (el, i) {
-    return i * 250;
+    return i * 350;
   },
   direction: "alternate",
-  loop: false
+  loop: false,
+  complete: function() {
+    // Add your next animation here
+    tree_branches.forEach(element => {
+      element.style.visibility = 'visible';
+    });
+
+    anime({
+
+      targets: ".container g #Tree > #Branches path",
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: "linear",
+      duration: 500,
+      delay: function (el, i) {
+        return i * 350;
+      },
+      direction: "alternate",
+      loop: false,
+    });
+  }
 });
