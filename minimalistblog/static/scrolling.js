5323891f53d516cf15ea7d703c304bd3b2a8f909
Mathias157 <mathiasros@gmail.com>
2024-08-13 21:34:45 +0200

    Attempt two

--- minimalistblog/static/scrolling.js
+++ minimalistblog/static/scrolling.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.section');
     window.currentSection = 0;
-    let touchStartX = 0;
-    let touchEndX = 0;
-
 
     // Function to scroll to the correct section
     function scrollToSection(sectionIndex) {
@@ -27,58 +24,47 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Function to handle wheel scroll events
-    // function handleWheelScroll(event) {
-    //     const delta = Math.sign(event.deltaY);
-    //     if (delta > 0) {
-    //         if (window.currentSection < sections.length - 1) {
-    //             window.currentSection++;
-    //             scrollToSection(window.currentSection);
-    //         }
-    //     } else if (delta < 0) {
-        //         if (window.currentSection > 0) {
-            //             window.currentSection--;
-            //             scrollToSection(window.currentSection);
-            //         }
-            //     }
-            // }
-            
-            // Function to handle touch start event
-            function handleTouchStart(event) {
-                touchStartX = event.touches[0].clientX;
+    document.addEventListener('touchstart', function (event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }, false);
+    
+    document.addEventListener('touchend', function (event) {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleGesture();
+    }, false);
+    
+    
+    function handleGesture() {
+        if (touchendX < touchstartX) {
+            if (window.currentSection < sections.length - 1) {
+                window.currentSection++;
+                scrollToSection(window.currentSection);
             }
-            
-            // Function to handle touch move event
-            function handleTouchMove(event) {
-                touchEndX = event.touches[0].clientX;
+        }
+    
+        if (touchendX > touchstartX) {
+            if (window.currentSection > 0) {
+                window.currentSection--;
+                scrollToSection(window.currentSection);
             }
-            
-            // Function to handle touch end event
-            function handleTouchEnd() {
-                const deltaX = touchEndX - touchStartX;
-                if (deltaX > 50) { // Swipe right
-                    if (window.currentSection > 0) {
-                        window.currentSection--;
-                        scrollToSection(window.currentSection);
-                    }
-                } else if (deltaX < -50) { // Swipe left
-                    if (window.currentSection < sections.length - 1) {
-                        window.currentSection++;
-                        scrollToSection(window.currentSection);
-                    }
-                }
-            }
-            
-            // Add event listener for wheel scroll
-            // document.addEventListener('wheel', handleWheelScroll, { passive: true });
-            
-            // Add event listeners for touch events
-            document.addEventListener('touchstart', handleTouchStart);
-            document.addEventListener('touchmove', handleTouchMove);
-            document.addEventListener('touchend', handleTouchEnd);
-            
-            // Add event listener for arrow key presses
-            document.addEventListener('keydown', handleKeyPress);
-            
-        });
-        
+        }
+    
+        // if (touchendY < touchstartY) {
+        //     console.log('Swiped Up');
+        // }
+    
+        // if (touchendY > touchstartY) {
+        //     console.log('Swiped Down');
+        // }
+    
+        // if (touchendY === touchstartY) {
+        //     console.log('Tap');
+        // }
+    }
+
+
+    // Add event listener for arrow key presses
+    document.addEventListener('keydown', handleKeyPress);
+});
