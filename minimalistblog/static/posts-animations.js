247447ed61121a0a983d923b76133ef21a9700df
Mathias157 <mathiasros@gmail.com>
2024-08-14 21:52:35 +0200

    Made new tree for videos and placeholders for the others

--- minimalistblog/static/posts-animations.js
+++ minimalistblog/static/posts-animations.js
@@ -2,23 +2,6 @@
 // The original scale of the fruit circles (posts)
 var $circle_init_scale = 1.25;
 
-// Post 3 custom thumbnail animation for PostTree1 (since it will grab the first instances, and ignore the once in PostTree2 and PostTree3)
-let post_part1 = document.querySelector('#Posts > #Post3 > #thumbnail #hvis_vinden');
-post_part1.style.opacity = 0;
-let post_part2 = document.querySelector('#Posts > #Post3 > #thumbnail #texture');
-post_part2.style.opacity = 0;
-let post_part3 = document.querySelector('#Posts > #Post3 > #thumbnail #vandrer_vider');
-post_part3.style.opacity = 0;
-let post3 = document.querySelector('#Post3 circle');
-// Set upper layer opacities to 1
-let post_thumbnail = document.querySelector('#Posts > #Post3 > #thumbnail');
-post_thumbnail.style.opacity = 1;
-
-let post3_animation = gsap.fromTo([post_part2, post_part1, post_part3],
-    {opacity: 0},
-    {opacity: 1, duration: 0.5, stagger: 0.1}
-);
-post3_animation.pause()
 
 // List to store fruit animations (posts)
 window.fruit_animations = [];
@@ -37,7 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let i = 1; i <= 3; i++) {
         
         const posts = [];
-        for (let j = 1; j < 6; j++) {
+        let N_posts = document.querySelectorAll(`#PostTree${i} #Posts > g`).length;
+        console.log(document.querySelectorAll(`#PostTree${i} #Posts > g`))
+        for (let j = 1; j <= N_posts; j++) {
             let post = document.querySelector(`#PostTree${i} #Post${j} circle`);
             
             post.style.visibility = 'visible';
@@ -47,40 +32,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 gsap.to(post, {scale: $circle_init_scale + 0.4, duration: 0.3, transformOrigin: "center"});
                 
                 // Make post information visible
-                const info_path = document.querySelector(`#PostTree${i} #Post${j} > g > path`)
-                info_path.style.opacity = 1;
-                if (j !== 3) {
-                    // Just show text, except for post 3
-                    const info_text = document.querySelector(`#PostTree${i} #Post${j} > g > text`)
-                    info_text.style.opacity = 1;
-                }
+                let info_text = document.querySelectorAll(`#PostTree${i} #Post${j} > g > text`)
+                info_text.forEach(element => {
+                    // console.log(`Making text visible for PostTree${i} Post${j}`);
+                    element.style.opacity = 1;
+                });
             })
             
             post.addEventListener('mouseout', function() {
                 gsap.to(post, {scale: $circle_init_scale, duration: 0.3, transformOrigin: "center"});
                 
-                // Make post information visible
-                const info_path = document.querySelector(`#PostTree${i} #Post${j} > g > path`)
-                info_path.style.opacity = 0;
-                if (j !== 3) {
-                    // Just show text, except for post 3
-                    const info_text = document.querySelector(`#PostTree${i} #Post${j} > g > text`)
-                    info_text.style.opacity = 0;
-                }
+                // Make post information invisible
+                let info_text = document.querySelectorAll(`#PostTree${i} #Post${j} > g > text`)
+                info_text.forEach(element => {
+                    // console.log(`Making text visible for PostTree${i} Post${j}`);
+                    element.style.opacity = 0;
+                });
             })
         }
-        
-        post3.addEventListener('mouseover', function() {
-            post3_animation.play();
-            
-        });
-        
-        post3.addEventListener('mouseout', function() {
-            // post_thumbnail.style.opacity = 0;
-            post3_animation.reverse();
-            
-        });
-        
+
         
         // Animation to make the posts appear with a staggered effect
         let fruit_animation = gsap.fromTo(posts, 
@@ -89,6 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 );
         window.fruit_animations.push(fruit_animation)
     }
+    
 });
 
 
